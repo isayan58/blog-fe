2e7441c1ecb4c3efcc5ba3831d9a7b2b4f75f53c
isayan58 <sayan.96.adhikary@gmail.com>
2021-11-15 03:03:38 +0530

    Search by author's first name is working fine.
    Search by author's first name is working fine. Need to add other fields to search for, properly.

--- src/Blogpost/BlogSearch.tsx
+++ src/Blogpost/BlogSearch.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 
 interface Blogs
 {
@@ -15,6 +15,7 @@ const BlogSearch = () =>
 {
     const { search } = useParams();
     //console.log(params.search);
+    const navigate = useNavigate();
     const [blogSearch, setBlogSearch] = useState<Blogs[]>([]);
     const searchBlog = () =>
     {
@@ -22,12 +23,35 @@ const BlogSearch = () =>
         {
             const apiResponse = await fetch(`http://localhost:8000/search/${search}`);
             const responseData = await apiResponse.json();
+            setBlogSearch(responseData);
         }
+        fetchBlog();
     }
     useEffect(() =>
         searchBlog(), [blogSearch]);
     return(
-        <div>{search}</div>
+    <div className="home-blogs">
+    {
+        blogSearch.length != 0 ?
+    blogSearch.map((data)=>
+    (
+        <div className="blogs-card" onClick={()=>navigate(`/blogs/${data.blogger_id}/${data.title}`)}>
+        <div className="image-container"><img src={data.header_image}
+        className="home-blog-image"/></div>
+        <div className="blog-details-container">
+            <div>{data.title}</div>
+        <div className="tags">{data.tags.map((tags)=>
+        (
+            <div>{tags} ; </div>
+        ))
+        }</div>
+        </div>
+    </div>
+    ))
+    :
+    <div>No Search Results yet.</div>
+    }
+</div>
     )
 }
 export default BlogSearch;
